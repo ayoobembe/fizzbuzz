c7ce273de1d6fcc979e6cd88eea8a69b6d17365e
danielobembe <ayo.obembe@gmail.com>
2014-10-27 17:51:46 +0000

    Building from scratch. Completed is_divisible_by_three?() method and test-cases

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,25 +1,15 @@
 def is_divisible_by_three?(number)
-	is_divisible_by?(number, 3)
+	is_divisible?(number, 3)
 end
 
 def is_divisible_by_five?(number)
-	is_divisible_by?(number,5)
+	true
 end
 
 
-def is_divisible_by_fifteen?(number)
-	is_divisible_by?(number,15)
+
+
+def is_divisible?(number, divisor)
+	number % divisor == 0	
 end
-
-def is_divisible_by?(number, divisor)
-	number % divisor == 0
-end
-
-def fizzbuzz_says(number)
-	return "Fizzbuzz" if is_divisible_by_fifteen?(number) 
-	return "Fizz" if is_divisible_by_three?(number) 
-	return "Buzz" if is_divisible_by_five?(number)
-end
-
-
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,55 +1,27 @@
-require 'fizzbuzz'
+#always require the class being tested
+require "fizzbuzz.rb"
 
-describe 'fizzbuzz' do
-	context 'knows when a number' do
+describe "Fizzbuzz" do
 
-		it 'is divisible by three' do
+	context "Knows when a number: " do
+
+		#Test cases for method: is_divisible_by_three?(number)
+		it "is divisible by three" do
 			expect(is_divisible_by_three?(3)).to be true
 		end
 
-		
-		it 'is divisible by five' do
-			expect(is_divisible_by_five?(5)). to be true
+		it "is not divisible by three" do
+			expect(is_divisible_by_three?(1)).to be false
 		end
 
-		it 'is divisible by fifteen' do
-			expect(is_divisible_by_fifteen?(15)). to be true
+
+		#Test cases for method: is_divisible_by_five?(number)
+		it "is divisible by five" do
+			expect(is_divisible_by_five?(5)).to be true
 		end
 
-		it 'is NOT divisible by three' do
-			expect(is_divisible_by_three?(1)). to be false
-		end
 
-		it 'is NOT divisible by five' do
-			expect(is_divisible_by_five?(1)). to be false
-		end
-		
-		it 'is NOT divisible by fifteen' do
-			expect(is_divisible_by_fifteen?(1)). to be false
-		end
 
 	end
-
+  
 end
-
-
-describe 'when playing the game says' do
-
-	it "'Fizz' when a number is divisible by 3" do
-		expect(fizzbuzz_says(3)).to eq "Fizz"
-	end
-
-	it "'Buzz' when a number is divisible by 5" do
-		expect(fizzbuzz_says(5)).to eq "Buzz"
-	end
-
-	it "'Fizzbuzz' when a number is divisible by 15" do
-		expect(fizzbuzz_says(15)).to eq "Fizzbuzz"
-	end
-
-
-end
-		
-
-
-
